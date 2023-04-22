0cbfba55cf64ba674fd89e47657d2636099bb042
Sterberino <91395511+Sterberino@users.noreply.github.com>
2023-04-21 18:55:07 -0700

    initial commit

---
+++ src/Assets/Components/NeoMorphicIcon.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../Styles/neomorphicStyles.css'
+
+export default function NeoMorphicIcon({iconFilepath, style, callback})
+{
+    const [clicked, setClicked] = React.useState(false);
+
+    function Toggle()
+    {
+        setClicked(prev => !prev);
+        if(callback)
+        {
+            callback();
+        }
+    }
+
+    return(
+        <div 
+            className= {clicked ? "neomorphic-button-flat-pressed" : "neomorphic-button-flat-unpressed"}
+            onClick={()=>{Toggle()}}   
+        >    
+            <img src= {`${process.env.PUBLIC_URL}${iconFilepath}${clicked ? '-selected' : ''}.png`}/>
+        </div>
+    )
+}
