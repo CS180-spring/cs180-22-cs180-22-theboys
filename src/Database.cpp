67e6f26b2d2783a80451f715827e885e304b5ae4
adrianram12 <adrianramos2121@gmail.com>
2023-05-04 15:47:18 -0700

    validate credentials from login file

--- src/Database.cpp
+++ src/Database.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstring>
+
+#include <pqxx/pqxx>
 
 using namespace std;
 
 int main(){
 
+    ifstream input("login.txt");
+
+    vector<string> credentials;
+
+    while(!input.eof()){
+
+        string line;
+        getline(input, line);
+        credentials.push_back(line);
+
+    }
 
     return 0;
 }
